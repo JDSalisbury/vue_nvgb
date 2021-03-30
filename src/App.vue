7bafe2e91d815fe98302272720908df917c2b9b8
Jeff <Salisbury.jeffery@gmail.com>
2021-03-29 22:55:12 -0400

    Attempting to setup a nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <div id="app">
-    <router-link to="/">
-      <img alt="Vue logo" src="./assets/chicken.png" />
-    </router-link>
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="white"
+        elevate-on-scroll
+        scroll-target="#scrolling-techniques-7"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <router-link to="/">
+          <img style="width: 30%" alt="Vue logo" src="./assets/chicken.png" />
+        </router-link>
+      </v-app-bar>
+      <v-sheet
+        id="scrolling-techniques-7"
+        class="overflow-y-auto"
+        max-height="600"
+      >
+        <v-container style="height: 100px"> </v-container>
+      </v-sheet>
+    </v-card>
     <router-view />
   </div>
 </template>
