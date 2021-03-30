3c1f6192912e42a9cb9851cd19bac0b56000d3c8
Jeff <Salisbury.jeffery@gmail.com>
2021-03-29 22:09:46 -0400

    Attempting to dynamically add blogs to router, clever, maybe too clever. good not great.

--- src/router.js
+++ src/router.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './views/HelloWorld.vue'
-import Foo from './components/test.md'
-import Blogs from './views/Blogs.vue'
+import Foo from './markdown/older/test.md'
+import Blog from './views/Blogs.vue'
+
+import BlogEntries from './static/blogs.json'
+
+
+
 Vue.use(VueRouter)
+
+const blogRoutes = Object.keys(BlogEntries).map(section => {
+    const children = BlogEntries[section].map(child => ({
+        path: child.id,
+        name: child.id,
+        component: () => import(`./markdown/${section}/${child.id}.md`)
+    }))
+    return {
+        path: `/${section}`,
+        name: section,
+        component: () => import('./views/Blog.vue'),
+        children
+    }
+})
 
 const routes = [
     { path: '/', name: 'HelloWorld', component: Home },
-    { path: '/blogs', name: 'Blogs', component: Blogs },
-    { path: '/foo', name: 'foo', component: Foo }
+    { path: '/blogs', name: 'Blogs', component: Blog },
+    { path: '/foo', name: 'foo', component: Foo },
+    ...blogRoutes
 ];
 
 const router = new VueRouter({
