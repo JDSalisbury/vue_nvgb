164aaa6452d1fe91d434eabd2dc3044d65d796f9
Jeff <Salisbury.jeffery@gmail.com>
2021-03-31 23:26:02 -0400

    Creating components, footer comp.

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer dark padless>
+    <v-col class="text-center">
+      {{ new Date().getFullYear() }} — <strong>NVGB</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<style scoped>
+footer {
+  height: 65px;
+}
+</style>
